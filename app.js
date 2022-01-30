1d416d0b40ebece83aed03e5c3a732f3c4a6bf82
Artur Michalak <artur93mm@gmail.com>
2022-01-30 18:10:45 +0100

    CSS index.html

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const menuList = document.querySelector(".page-menu_list");
 
 const menuSlide = () =>{
     burger.addEventListener('click', () =>{
+        console.log("click");
         menuList.classList.toggle("burger-active");
     })
 }
+
+menuSlide()
