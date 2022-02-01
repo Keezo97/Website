d6901d40ccd52607aab8e821883920b9f3f6188a
Artur Michalak <artur93mm@gmail.com>
2022-02-01 21:30:11 +0100

    krew pot i łzy - koniec

--- app.js
+++ app.js
@@ -1,3 +1,31 @@
+const plusMinus = () =>{
+    const plus = document.querySelector(".plus");
+    const minus = document.querySelector(".minus");
+    const cost = document.querySelector(".cost");
+    const peopleAmount = document.querySelector(".peopleAmount");
+    
+    if(plus && minus) {
+        plus.addEventListener('click', () =>{
+            let text = parseInt(peopleAmount.textContent);
+            if(text < 5) {
+                text = text + 1;
+                peopleAmount.innerText = text;
+                cost.innerText = `€${text * 2300}`;
+            }
+        })
+        minus.addEventListener('click', () =>{
+            let text = parseInt(peopleAmount.textContent);
+            if (text > 1){
+                text = text - 1;
+                peopleAmount.innerText = text;
+                cost.innerText = `€${text * 2300}`;
+            }
+    })
+}
+}
+
+plusMinus();
+
 // Burger
 
 const burger = document.querySelector(".burger");
@@ -26,8 +54,50 @@ let currentDay = `${day}-${month}-${year}`;
 date.innerText = currentDay;
 
 //Slider
-var elem = document.querySelector('.opinions-wrapper');
-var flkty = new Flickity( elem, {
+let elem = document.querySelector('.opinions-wrapper');
+let flkty = new Flickity( elem, {
   cellAlign: 'center',
   contain: true
 });
+
+//Form validation
+const valid = () =>{
+    const nam = document.getElementById("name"); 
+    const email = document.getElementById("email"); 
+    const number = document.getElementById("number"); 
+    const form = document.getElementById("form"); 
+    const messageArea = document.getElementById("messageArea");
+    const errorText = document.getElementById("errorText");
+
+    if(form) {
+        form.addEventListener('submit', (e)=>{
+            e.preventDefault()
+            let messages = [];
+            if(nam.value === '' || nam.value == null) {
+                messages.push('Imie jest wymagane')
+            }
+            
+            if(email.value.indexOf("@") === -1 ) {
+                messages.push('Email nieprawidłowy')
+            }
+
+            if(number.value.length != 9) {
+                messages.push('Numer nieprawidłowy')
+            }
+
+            if (messages.length > 0) {
+                errorText.style.color = "red";
+                errorText.innerText = messages.join(", ");
+            }
+            else {
+                errorText.style.color = "green"
+                errorText.innerText = "Wiadomość została wysłana";
+                nam.value = '';
+                email.value = '';
+                number.value = '';
+                messageArea.value = '';  
+            }
+    })}
+}
+
+valid()
